610674af7c6bb8001eaab38c9a58a4528a1de4fc
lv <781262125@qq.com>
2018-11-20 22:16:47 +0800

    11.20 UI

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager Instance;
+    public GameObject coin;
+    public Animator animator;
+    public int count;
+    public bool isBegin;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        InitCoin();
+        count = 0;
+        isBegin = false;
+    }
+
+    private void InitCoin()
+    {
+        Vector3 origin = new Vector3(-6f, 0.5f, 0f);
+        bool x = true;
+        bool z = true;
+        for (int i = 0; i < 12; ++i)
+        {
+            Instantiate(coin, origin, coin.transform.rotation);
+            if (x)
+            {
+                if (origin.x != 6f)
+                {
+                    origin.x += 2f;
+                }
+                else
+                {
+                    x = false;
+                    origin.x -= 2f;
+                }
+            }
+            else
+            {
+                origin.x -= 2f;
+            }
+            if (z)
+            {
+                if (origin.z != 6f)
+                {
+                    origin.z += 2f;
+                }
+                else
+                {
+                    z = false;
+                    origin.z -= 2f;
+                }
+            }
+            else
+            {
+                if (origin.z != -6f)
+                {
+                    origin.z -= 2f;
+                }
+                else
+                {
+                    z = true;
+                    origin.z += 2f;
+                }
+            }
+        }
+    }
+
+    public void Begin()
+    {
+        animator.SetTrigger("out");
+        isBegin = true;
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 10f;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    void FixedUpdate ()
+    {
+        if (!GameManager.Instance.isBegin)
+            return;
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        rb.AddForce(new Vector3(horizontal, 0.0f, vertical) * speed);
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Coin"))
+        {
+            other.gameObject.SetActive(false);
+            GameManager.Instance.count += 1;
+        }
+    }
+}
